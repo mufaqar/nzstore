04079aef7f8b8881ff271acc043316a1ad5f12b6
Mufaqar Islam <mufaqar@gmail.com>
2022-10-07 21:25:57 +0500

    updated

--- super/partials/content-order.php
+++ super/partials/content-order.php
@@ -25,7 +25,14 @@
                                 $ticket_id = get_post_meta(get_the_ID(), 'order_id', true); 
                                 $ticket_agent = get_post_meta($ticket_id, 'order_uid', true); 
                                 $user_id = get_post_meta( $ticket_id, 'order_uid', true );
-                                $user_info = get_userdata($user_id);                                                         
+                                $user_info = get_userdata($user_id);  
+                                $cat_status  = get_the_terms( $pid, 'order_status');                                 
+                           
+                                
+                                foreach($cat_status as $cat_selected)
+                                {
+                                  $cat_active =  $cat_selected->slug;
+                                }  
                                 ?>
                                 <tr>
                                     <td><?php echo $i ?></td>
@@ -34,7 +41,7 @@
                                     <td><?php  echo  $user_info->user_login ?></td>                                  
                                     <td><?php  echo  get_the_modified_date(); ?></td>                          
                                     <td>$ <?php echo get_post_meta(get_the_ID(), 'order_price', true); ?></td>
-                                    <td><span class="status <?php echo get_post_meta(get_the_ID(), 'order_status', true); ?>"><?php echo get_post_meta(get_the_ID(), 'order_status', true); ?> </span> </td>
+                                    <td><span class="status <?php  echo $cat_active ?>"><?php echo $cat_active ?> </span> </td>
                                     <td> <a href="<?php echo home_url('admin-dashboard/edit-invoice?id='.$pid.''); ?>">Edit </a>  <i class="fa-solid fa-down-to-line"></i></td>
                                 </tr>
                             <?php endwhile;
