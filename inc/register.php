2a5975f9eee958b8a1f55401d5e88a6a6b57c212
Mufaqar Islam <mufaqar@gmail.com>
2022-10-09 19:22:40 +0500

    updated code

--- inc/register.php
+++ inc/register.php
@@ -73,36 +73,36 @@
 
 
 
-add_action('wp_ajax_resetpassword', 'resetpassword', 0);
-add_action('wp_ajax_nopriv_resetpassword', 'resetpassword');
-function resetpassword() {	
-	  $username = stripcslashes($_POST['username']);	
-	  $password = generateRandomString();		
-	  global $wpdb;  
-    //We shall SQL escape all inputs  
-      $username = $_POST['username'];
-      $email = $_POST['username'];    
-	  $password = generateRandomString();	
-	  $user_data = array(
-		'user_login' => $username,
-		'user_email' => $username,
-		'user_pass' => $password,	
+// add_action('wp_ajax_resetpassword', 'resetpassword', 0);
+// add_action('wp_ajax_nopriv_resetpassword', 'resetpassword');
+// function resetpassword() {	
+// 	  $username = stripcslashes($_POST['username']);	
+// 	  $password = generateRandomString();		
+// 	  global $wpdb;  
+//     //We shall SQL escape all inputs  
+//       $username = $_POST['username'];
+//       $email = $_POST['username'];    
+// 	  $password = generateRandomString();	
+// 	  $user_data = array(
+// 		'user_login' => $username,
+// 		'user_email' => $username,
+// 		'user_pass' => $password,	
 		
-		);
+// 		);
 
-		$user = get_user_by( 'email', $email );
-		$user_id = $user->ID;
-	    $user_id = wp_update_user( array ( 'ID' => $user_id, 'user_pass' => $password ) );	
-	  	if (!is_wp_error($user_id)) {		    
-			sendmail($username,$password);
-			echo wp_send_json( array('code' => 200 , 'message'=>__('Password Updated , Please check your email')));
-	  	} else {	    		         
-			  echo wp_send_json( array('code' => 0 , 'message'=>__('Error Occured please check your email address')));
-	      	}
+// 		$user = get_user_by( 'email', $email );
+// 		$user_id = $user->ID;
+// 	    $user_id = wp_update_user( array ( 'ID' => $user_id, 'user_pass' => $password ) );	
+// 	  	if (!is_wp_error($user_id)) {		    
+// 			sendmail($username,$password);
+// 			echo wp_send_json( array('code' => 200 , 'message'=>__('Password Updated , Please check your email')));
+// 	  	} else {	    		         
+// 			  echo wp_send_json( array('code' => 0 , 'message'=>__('Error Occured please check your email address')));
+// 	      	}
 	  	
-	die;   	
+// 	die;   	
 		
-}
+// }
 
 
 
