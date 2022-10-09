80df306f8a7ae7ab347660fa10623c5e4dfa0a28
Mufaqar Islam <mufaqar@gmail.com>
2022-10-09 19:52:57 +0500

    updated emails

--- inc/ajax_request.php
+++ inc/ajax_request.php
@@ -64,26 +64,19 @@ function add_ticket()
 		$agent_email = $user->user_email;
 		sendmail($agent_email,"New Ticket Created by $agent_email ", $inserted_post_id);
 
-
 	    $image_url        = $file_url; // Define the image URL here
 		$image_name       = $file_name;
 		$upload_dir       = wp_upload_dir(); // Set upload folder
 		$image_data       = file_get_contents($image_url); // Get image data
 		$unique_file_name = wp_unique_filename( $upload_dir['path'], $image_name ); // Generate unique name
 		$filename         = basename( $unique_file_name ); // Create image file name
-		
-		// Check folder permission and define file location
 		if( wp_mkdir_p( $upload_dir['path'] ) ) {
 			$file = $upload_dir['path'] . '/' . $filename;
 		} else {
 			$file = $upload_dir['basedir'] . '/' . $filename;
 		}
-		// Create the image  file on the server
 		file_put_contents( $file, $image_data );
-			// Check image file type
 		$wp_filetype = wp_check_filetype( $filename, null );
-		
-		// Set attachment data
 		$attachment = array(
 			'post_mime_type' => $wp_filetype['type'],
 			'post_title'     => sanitize_file_name( $filename ),
@@ -93,25 +86,19 @@ function add_ticket()
 
 
 
-	if (!is_wp_error($inserted_post_id)) {
-
-		
+	if (!is_wp_error($inserted_post_id)) {	
 
 		// Create the attachment
 		$attach_id = wp_insert_attachment( $attachment, $file, $inserted_post_id );
-		 // Include image.php
 		 require_once(ABSPATH . 'wp-admin/includes/image.php');
-		   // Define attachment metadata
-		   $attach_data = wp_generate_attachment_metadata( $attach_id, $file );
-		    // Assign metadata to attachment
-		  wp_update_attachment_metadata( $attach_id, $attach_data );
-
-		   // And finally assign featured image to post
-		   set_post_thumbnail( $inserted_post_id, $attach_id );
-		//sendmail($username,$password);
+		 $attach_data = wp_generate_attachment_metadata( $attach_id, $file );
+		wp_update_attachment_metadata( $attach_id, $attach_data );
+		set_post_thumbnail( $inserted_post_id, $attach_id );
 		echo wp_send_json(array('code' => 200, 'message' => __('Ticket Created Sucessfully')));
+		die();
 	} else {
 		echo wp_send_json(array('code' => 0, 'message' => __('Error Occured please fill up form carefully.')));
+		die();
 	}
 
 	die;
