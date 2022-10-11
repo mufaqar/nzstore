c5ce808aff55f5dcc8173966fcafb8d966fb2b97
Mufaqar Islam <mufaqar@gmail.com>
2022-10-11 20:03:26 +0500

    upload composer

--- temp-test.php
+++ temp-test.php
@@ -6,22 +6,21 @@
 get_header('landing');
 
 
-require "vendor/autoload.php";
-
-use Spipu\Html2Pdf\Html2Pdf;
-
-$html2pdf = new Html2Pdf();
-$html2pdf->writeHTML('<h1>HelloWorld</h1>This is my first test');
-$html2pdf->output();
 
 
 
-// $html2pdf = new Html2Pdf();
-// $html2pdf->writeHTML('<h1 style="color:pink;">CodeWall PDF</h1> <br/> <p>Convert this HTML to PDF please!</p>');
-// $html2pdf->output('myPdf.pdf); // Generate and load the PDF in the browser.
+// (A) LOAD MPDF
+
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+$mpdf = new \Mpdf\Mpdf();
+$mpdf->WriteHTML('<h1>Hello world!</h1>');
+$mpdf->Output();
 
 
 ?>
+
 
 <?php get_footer();?>
 
