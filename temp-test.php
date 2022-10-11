1f02eebb3006485eb690dac2355c3daa025720cb
Mufaqar Islam <mufaqar@gmail.com>
2022-10-11 20:05:58 +0500

    upload

--- temp-test.php
+++ temp-test.php
@@ -15,8 +15,13 @@ get_header('landing');
 require_once __DIR__ . '/vendor/autoload.php';
 
 $mpdf = new \Mpdf\Mpdf();
+
+
 $mpdf->WriteHTML('<h1>Hello world!</h1>');
 $mpdf->Output();
+
+ob_clean();
+flush();
 
 
 ?>
