5aaa08af750633fa15f60d5a7edabaf0b4bb98ca
Mufaqar Islam <mufaqar@gmail.com>
2022-10-13 11:37:08 +0500

    updated

--- super/super_dashboard.php
+++ super/super_dashboard.php
@@ -8,6 +8,7 @@
                 <div class="catering_menu all-tickets buttons">
                     <a id="1" class="showSingle _active" target="1" data="">All Tickets</a>
                     <a id="2" class="showSingle" target="2" data="Quotation">Quotation</a>
+                    <a id="5" class="showSingle" target="5" data="Quotation-sent">Quotation Sent</a>
                     <a id="3" class="showSingle" target="2" data="Approval">Approval</a>
                     <a id="4" class="showSingle" target="2" data="Completed">Completed</a>
                 </div>
