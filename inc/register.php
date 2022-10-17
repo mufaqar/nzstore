af8c913c76a32e0393544e5d2b467b6159bd93f6
Mufaqar Islam <mufaqar@gmail.com>
2022-10-17 23:14:05 +0500

    updated

--- inc/register.php
+++ inc/register.php
@@ -1,108 +1,111 @@
 <?php
 
 
-// function generateRandomString($length = 10) {
-// 	$characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-// 	$charactersLength = strlen($characters);
-// 	$randomString = '';
-// 	for ($i = 0; $i < $length; $i++) {
-// 		$randomString .= $characters[rand(0, $charactersLength - 1)];
-// 	}
-// 	return $randomString;
-// }
+function generateRandomString($length = 10) {
+	$characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+	$charactersLength = strlen($characters);
+	$randomString = '';
+	for ($i = 0; $i < $length; $i++) {
+		$randomString .= $characters[rand(0, $charactersLength - 1)];
+	}
+	return $randomString;
+}
 
-// function sendmail($to,$password) {
-// 	$to = $to;
-// 	$admin = 'hei@doubledowndish.no';
-// 	$subject = 'Double Down Dish | Username & Password';
-// 	$body  = "<p><strong> Username :  </strong> $to </p> <p> <strong> Password : </strong> $password  </p>";
-// 	$headers = array('Content-Type: text/html; charset=UTF-8');	
-// 	$headers  = "From: " . $admin . "\r\n";
-// 	$headers .= "Reply-To: " . $to . "\r\n";		
-// 	$headers .= "MIME-Version: 1.0\r\n";
-// 	$headers .= "Content-Type: text/html; charset=UTF-8\r\n";
-// 	mail( $to, $subject, $body, $headers );
-// 	}
-
-
-
-// add_action('wp_ajax_usersignup', 'usersignup', 0);
-// add_action('wp_ajax_nopriv_usersignup', 'usersignup');
+function sendmail($to,$password) {
+	$to = $to;
+	$admin = 'hei@doubledowndish.no';
+	$subject = 'Double Down Dish | Username & Password';
+	$body  = "<p><strong> Username :  </strong> $to </p> <p> <strong> Password : </strong> $password  </p>";
+	$headers = array('Content-Type: text/html; charset=UTF-8');	
+	$headers  = "From: " . $admin . "\r\n";
+	$headers .= "Reply-To: " . $to . "\r\n";		
+	$headers .= "MIME-Version: 1.0\r\n";
+	$headers .= "Content-Type: text/html; charset=UTF-8\r\n";
+	mail( $to, $subject, $body, $headers );
+	}
 
 
-// function usersignup() {	
 
-// 	  //require_once('../../../wp-config.php');
-// 	  global $wpdb;
+add_action('wp_ajax_agent_signup', 'agent_signup', 0);
+add_action('wp_ajax_nopriv_agent_signup', 'agent_signup');
 
 
-		
-// 	  $username = ($_POST['username']);
-//       $email = ($_POST['username']);
-//       $phone = stripcslashes($_POST['phone']);
-//       $referral = stripcslashes($_POST['referral']);	
-//       $name = stripcslashes($_POST['name']);  
-// 	  $password = generateRandomString();	
-// 	  $user_data = array(
-// 		'user_login' => $username,
-// 		'user_email' => $email,
-// 		'user_pass' => $password,	
-// 		'display_name' => $name,
-// 		'role' => 'company'
-// 		);
-// 	    $user_id = wp_insert_user($user_data);
-// 	  	if (!is_wp_error($user_id)) {		    
-// 			sendmail($username,$password);
-// 			echo wp_send_json( array('code' => 200 , 'message'=>__('We have Created an account for you.')));
+function usersignup() {	
 
-// 	  	} else {
-// 	    	if (isset($user_id->errors['empty_user_login'])) {
+	  //require_once('../../../wp-config.php');
+	  global $wpdb;
+
+      $username = $_POST['agent_email'];
+      $agent_email = $_POST['agent_email'];
+      $agent_name = $_POST['agent_name'];
+      $business_address = $_POST['business_address'];	
+      $business_name = $_POST['business_name'];  
+      $business_phone = $_POST['business_phone'];  
+      $postal_code = $_POST['postal_code'];  
+	  $password = generateRandomString();	
+	  $user_data = array(
+		'user_login' => $username,
+		'user_email' => $agent_email,
+		'user_pass' => $password,	
+		'display_name' => $agent_name,
+		'role' => 'agent'
+		);
+	    $user_id = wp_insert_user($user_data);
+	  	if (!is_wp_error($user_id)) {	
+            update_user_meta( $user_id,'business_name', $business_name);	 
+            update_user_meta( $user_id,'business_phone', $business_phone);	  
+            update_user_meta( $user_id,'postal_code', $postal_code);	    
+			//sendmail($username,$password);
+			echo wp_send_json( array('code' => 200 , 'message'=>__('We have Created an account for you.')));
+
+	  	} else {
+	    	if (isset($user_id->errors['empty_user_login'])) {
 	          
-// 			  echo wp_send_json( array('code' => 0 , 'message'=>__('User Name and Email are mandatory')));
-// 	      	} elseif (isset($user_id->errors['existing_user_login'])) {
-// 	         // echo 'User name already exixts.';
-// 			  echo wp_send_json( array('code' => 0 , 'message'=>__('This email address is already registered.')));
-// 	      	} else {	         
-// 			  echo wp_send_json( array('code' => 0 , 'message'=>__('Error Occured please fill up the sign up form carefully.')));
-// 	      	}
-// 	  	}
-// 	die;   
+			  echo wp_send_json( array('code' => 0 , 'message'=>__('User Name and Email are mandatory')));
+	      	} elseif (isset($user_id->errors['existing_user_login'])) {
+	         // echo 'User name already exixts.';
+			  echo wp_send_json( array('code' => 0 , 'message'=>__('This email address is already registered.')));
+	      	} else {	         
+			  echo wp_send_json( array('code' => 0 , 'message'=>__('Error Occured please fill up the sign up form carefully.')));
+	      	}
+	  	}
+	die;   
 		
-// }
+}
 
 
 
 
-// add_action('wp_ajax_resetpassword', 'resetpassword', 0);
-// add_action('wp_ajax_nopriv_resetpassword', 'resetpassword');
-// function resetpassword() {	
-// 	  $username = stripcslashes($_POST['username']);	
-// 	  $password = generateRandomString();		
-// 	  global $wpdb;  
-//     //We shall SQL escape all inputs  
-//       $username = $_POST['username'];
-//       $email = $_POST['username'];    
-// 	  $password = generateRandomString();	
-// 	  $user_data = array(
-// 		'user_login' => $username,
-// 		'user_email' => $username,
-// 		'user_pass' => $password,	
+add_action('wp_ajax_resetpassword', 'resetpassword', 0);
+add_action('wp_ajax_nopriv_resetpassword', 'resetpassword');
+function resetpassword() {	
+	  $username = stripcslashes($_POST['username']);	
+	  $password = generateRandomString();		
+	  global $wpdb;  
+    //We shall SQL escape all inputs  
+      $username = $_POST['username'];
+      $email = $_POST['username'];    
+	  $password = generateRandomString();	
+	  $user_data = array(
+		'user_login' => $username,
+		'user_email' => $username,
+		'user_pass' => $password,	
 		
-// 		);
+		);
 
-// 		$user = get_user_by( 'email', $email );
-// 		$user_id = $user->ID;
-// 	    $user_id = wp_update_user( array ( 'ID' => $user_id, 'user_pass' => $password ) );	
-// 	  	if (!is_wp_error($user_id)) {		    
-// 			sendmail($username,$password);
-// 			echo wp_send_json( array('code' => 200 , 'message'=>__('Password Updated , Please check your email')));
-// 	  	} else {	    		         
-// 			  echo wp_send_json( array('code' => 0 , 'message'=>__('Error Occured please check your email address')));
-// 	      	}
+		$user = get_user_by( 'email', $email );
+		$user_id = $user->ID;
+	    $user_id = wp_update_user( array ( 'ID' => $user_id, 'user_pass' => $password ) );	
+	  	if (!is_wp_error($user_id)) {		    
+			sendmail($username,$password);
+			echo wp_send_json( array('code' => 200 , 'message'=>__('Password Updated , Please check your email')));
+	  	} else {	    		         
+			  echo wp_send_json( array('code' => 0 , 'message'=>__('Error Occured please check your email address')));
+	      	}
 	  	
-// 	die;   	
+	die;   	
 		
-// }
+}
 
 
 
--- temp-signup_agreement.php
+++ temp-signup_agreement.php
@@ -1,199 +1,103 @@
-<?php /* Template Name: Agent Signup  */ 
-
-get_header();
-
-reddirectProfile();
-
-
-
-
-
-?>  <main class="business_agreement">
+<?php /* Template Name: AgentSignup  */ 
+        get_header();
+        //reddirectProfile();
+                ?> 
+ <main class="business_agreement">
         <div class="agreement_wrapper d-flex position-relative justify-content-center flex-column align-items-center p-4">
-        <a href="<?php echo home_url(); ?>"> <img src="<?php bloginfo('template_directory'); ?>/reources/images/logo.png" class="logo" alt="logo"></a>
-            <div class="agreement_steps d-flex justify-content-center align-items-center mt-4 mt-lg-5 mb-lg-5">
-                <div class="step step_one"></div>
-                <div class="step step_two"></div>
-                <div class="step step_three"></div>
-            </div>
-        <form class="addprofile" id="profileform" action="#" > 
-            <div class="first_step step_wrapper d-flex justify-content-center flex-column align-items-center text-center">
-            <a href="<?php echo home_url(); ?>"> <img src="<?php bloginfo('template_directory'); ?>/reources/images/left arrow.png" class="arrow position-absolute" alt="back arrow"> </a>
-                <p>REGISTER FOR YOUR SMARTPHONES, LAPTOPS, IPAD, MACBOOKS & PERIPHERALS 							
-</p>
-                <div class="pl-4 pr-4">
-                    <h2 class="">Create Agent Profile</h2>
-                    <div class="form-group w-100">
-                        <label for="compnay_name">Your Name</label>
-                        <input type="text" class="form-control" id="compnay_name" placeholder="Company Name" required >
-                    </div>
-
-                    <div class="form-group w-100 mt-3 mb-5">
-                        <label for="compnay_delivery_address">Email adress</label>
-                        <input type="text" class="form-control" id="compnay_delivery_address" rows="3"
-                            placeholder="Enter Delivery adress" />
-                        <!-- <input type="text" class="form-control" id="compnay_agreement" 
-                            placeholder="Agreement Title" > -->
-                    </div>
-
-                    <a type="next" class="btn_primary d-block next" onclick="stepOne()">Continue</a>
-                </div>
-            </div>
-
-            <!-- step 2  -->
-            <div class="secound_step step_wrapper d-flex justify-content-center flex-column align-items-center text-center">
-                <img src="<?php bloginfo('template_directory'); ?>/reources/images/left arrow.png" class="arrow position-absolute" alt="back arrow"
-                    onclick="backToStepOne()">
-
-                <div class="pl-4 inner-content pr-4">
-                    <h2 class=""> Busniess Details</h2>
-              
-                    
-                   
-
-                    <div class="form-group w-100  mb-2">
-                        <label for="compnay_delivery_address">Business Address</label>
-                        <input type="text" class="form-control" id="starting_date" value="<?php echo $date ?>" aria-describedby="emailHelp"
-                                placeholder="Enter your business address" >
-                    </div>
-                    <div class="form-group w-100  mb-2">
-                        <label for="compnay_delivery_address">Business Phone Number</label>
-                        <input type="text"  class="form-control" id="compnay_delivery_address" rows="3"
-                            placeholder="Enter Delivery phone number" />
-                            </div>
-                    <div class="form-group w-100  mb-2">
+                <a href="<?php echo home_url(); ?>"> <img src="<?php bloginfo('template_directory'); ?>/reources/images/logo.png" class="logo" alt="logo"></a>
+                
+                <form class="add_agent" id="add_agent" action="#" > 
+                    <div class="d-flex justify-content-center flex-column align-items-center text-center">
+                    <a href="<?php echo home_url(); ?>"> <img src="<?php bloginfo('template_directory'); ?>/reources/images/left arrow.png" class="arrow position-absolute" alt="back arrow"> </a>
+                    <h2 class="">Create Agent Profile</h2>   
+                    <p>REGISTER FOR YOUR SMARTPHONES, LAPTOPS, IPAD, MACBOOKS & PERIPHERALS</p>
+                        <div class="pl-4 pt-3 pr-4">
                             
-                            <label for="compnay_delivery_address">Business Name</label>
-                    <input type="text" class="form-control" id="starting_date" value=""   placeholder="Enter Your Business Name" >
-                    </div>
-                    <div class="form-group w-100  mb-2">
-                            <label for="compnay_delivery_address">Postal Code</label>
-                    <input type="text" class="form-control" id="starting_date" value=""
-                            placeholder="Enter Your Postal Code" >
-                    </div>
+                        <div class="form-group w-100  mb-2">
+                                <label for="compnay_name">Your Name</label>
+                                <input type="text" class="form-control" id="agent_name" placeholder="Agent Name" required >
+                            </div>
+                            <div class="form-group w-100  mb-2">
+                                <label for="compnay_delivery_address">Email adress</label>
+                                <input type="text" class="form-control" id="agent_email"  placeholder="Enter Your Email adress" />                      
+                            </div>
 
-                 
-
-                    <a type="next" class="btn_primary d-block next" onclick="stepTwo()">Continue</a>
-                </div>
+                            <div class="form-group w-100  mb-2">
+                                <label for="business_address">Business Address</label>
+                                <input type="text" class="form-control" id="business_address" placeholder="Enter your business address" >
+                            </div>
+                            <div class="form-group w-100  mb-2">
+                                <label for="business_phone">Business Phone Number</label>
+                                <input type="text"  class="form-control" id="business_phone" placeholder="Enter Business Phone Number" />
+                                    </div>
+                            <div class="form-group w-100  mb-2">                            
+                                    <label for="business_name">Business Name</label>
+                            <input type="text" class="form-control" id="business_name"  placeholder="Enter Your Business Name" >
+                            </div>
+                            <div class="form-group w-100  mb-2">
+                                    <label for="postal_code">Postal Code</label>
+                            <input type="text" class="form-control" id="postal_code"  placeholder="Enter Your Postal Code" >
+                            </div>
+                            <div class="pinfo mb-5">
+                                    
+                                    <p class="text">Thanks for subscribing us and welcome to be the business partner for all of your <p>
+                                    <p>Smartphones, laptops, iPad, and  MacBooks are repaired at competitive prices. After repair warranty
+                                        Special discount for the business  having turn over more than $5000/month </p>
+                                        <p class="text">  We at BUDGET COMPUTERS AND KIWI MOBILES, HAVING HIGHLY SKILLED TECHNICIANS, QUALIFIED FROM NEW ZEALAND POLYTECH IN IT. WE HAVE HI-TECH LAB WITH LATEST TOOLS TO DIAGNOSE THE FAULY AND REPAIR, WE ALSO SPECIALISED IN MOTHERBOARDS 
+                                        DIAGNOSTIC AND PART REPLACEMENTS, i.e., IC, VGA card, data recovery, charging slot repair.</p>
+                                        <p class="text"> One of our sales team will contact you at your preferred time to discuss more and answer your queries </p>
+                            </div>
+                            <button type="submit" class="btn_primary d-block next">Sign Up</button>                   
+                        </div>
+                    </div>      
+                </form>
+                <div id="last_step">
+                    <div  class="finish_step step_wrapper d-flex justify-content-center flex-column align-items-center text-center">            
+                        <div class="content mt-5">
+                            <div class="right"><img src="<?php bloginfo('template_directory'); ?>/reources/images/img 3.png" alt=""></div>
+                            <h1 class="finished">Finished!</h1>
+                            <h2 class="looking">Your Request Sent</h2>
+                            <p class="find_information">We have now sent you an Email for varification. Please check and activate your account</p>             
+                            <a href="<?php echo home_url(); ?>" class="btn_primary mb-5">Go to the front</a>
+                        </div>                    
+                    </div>
+                </div>    
             </div>
 
-            <!-- step 3  -->
-            <div
-                class="third_step step_wrapper d-flex justify-content-center flex-column align-items-center text-center">
-                <img src="<?php bloginfo('template_directory'); ?>/reources/images/left arrow.png" class="arrow position-absolute" alt="back arrow"
-                    onclick="backToStepTwo()">
-
-                <div class="pl-4 third pr-4">
-
-                 
-
-                    <div class="pinfo mb-5">
-                        <h2 class="">Term of use, privacy andcrelevant information</h2>
-                        <p class="text">Thanks for subscribing us and welcome to be the business partner for all of your
-
-                                Smartphones, laptops, iPad, and  MacBooks are repaired at competitive prices. After repair warranty
-                                Special discount for the business  having turn over more than $5000/month </p>
-                                <p class="text">  We at BUDGET COMPUTERS AND KIWI MOBILES, HAVING HIGHLY SKILLED TECHNICIANS, QUALIFIED FROM NEW ZEALAND POLYTECH IN IT. WE HAVE HI-TECH LAB WITH LATEST TOOLS TO DIAGNOSE THE FAULY AND REPAIR, WE ALSO SPECIALISED IN MOTHERBOARDS 
-                                DIAGNOSTIC AND PART REPLACEMENTS, i.e., IC, VGA card, data recovery, charging slot repair.</p>
-
-                                <p class="text"> One of our sales team will contact you at your preferred time to discuss more and answer your queries 					
-									
-									
-                        </p>
-                    </div>
-
-                    <div class="d-flex align-items-center mb-2">
-                        <p class="">
-                            <input type="radio" id="test1" name="radio-group" checked>
-                            <label for="test1">Yes, Please</label>
-                        </p>
-                        <p style="margin-left: 2rem;">
-                            <input type="radio" id="test2" name="radio-group">
-                            <label for="test2">No Thanks</label>
-                        </p>
-                    </div>
-
-                    <button type="next" class="btn_primary d-block next">Complete</button>
-                </div>
-            </div>
-
-      
-        </form>
-        <div id="last_step">
-            <div  class="finish_step step_wrapper d-flex justify-content-center flex-column align-items-center text-center">            
-                <div class="content mt-5">
-                    <div class="right"><img src="<?php bloginfo('template_directory'); ?>/reources/images/img 3.png" alt=""></div>
-                    <h1 class="finished">Finished!</h1>
-                    <h2 class="looking">We Looking forward to make lunch with you</h2>
-                    <p class="find_information">We have now sent you an Email where you will find information on how to login
-                        and manage your company and your orders.Companies receive an invoice at the end of each month.
-                    </p>
-                    <h3 class="employees_receive">Employees receive it at the end of each week</h3>
-                    <a href="<?php echo home_url(); ?>" class="btn_primary mb-5">Go to the front</a>
-                </div>                    
-            </div>
-
-        </div>
-
-
-    
-</div>
-
-</main>
-
-
-
-
+    </main>
 
 
     <?php get_footer();?>
-
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-
     <script type="text/javascript">   
      jQuery(document).ready(function($) {				
-        $("#profileform").submit(function(e) {               
-            e.preventDefault();
-            var username = jQuery('#username').val();
-            var name = jQuery('#name').val();          
-            var phone = jQuery('#phone').val();	             
-            var compnay_name = jQuery('#compnay_name').val();	 
-            var compnay_delivery_address = jQuery('#compnay_delivery_address').val();	 
-            //var compnay_agreement = jQuery('#compnay_agreement').val();	            
-            var start_date = jQuery('#start_date').val();      
-            var lunch_benefit = jQuery('#lunch_benefit').val();	 
-            var lunch_benfit_type = jQuery('#lunch_benfit_type').val();	 
-            var invite_user1 = jQuery('#invite_user1').val();	 
-            var invite_user2 = jQuery('#invite_user2').val();	
-            var invite_user3 = jQuery('#invite_user3').val();	 
-
+        $("#add_agent").submit(function(e) {               
+            e.preventDefault();           
+            var agent_name = jQuery('#agent_name').val();  
+            var agent_email = jQuery('#agent_email').val();        
+            var business_address = jQuery('#business_address').val();	
+            var business_name = jQuery('#business_name').val();	
+            var business_phone = jQuery('#business_phone').val();	 
+            var postal_code = jQuery('#postal_code').val();	  
             $.ajax(
                 {
                     type:"POST",
                     url:"<?php echo admin_url('admin-ajax.php'); ?>",
                     data: {
-                        action: "companysignup",
-                        username : username,
-                        name : name,
-                        phone : phone,
-                        compnay_name : compnay_name,                  
-                        compnay_delivery_address : compnay_delivery_address,
-                        lunch_benfit_type : lunch_benfit_type,
-                        lunch_benefit : lunch_benefit,
-                        invite_user1 : invite_user1,
-                        invite_user2 : invite_user2,
-                        invite_user3 : invite_user3,
-                        start_date : start_date                     
-
+                        action: "agent_signup",
+                        agent_email : agent_email,
+                        agent_name : agent_name,
+                        business_address : business_address,
+                        business_name : business_name, 
+                        business_phone : business_phone,  
+                        postal_code : postal_code
                     },   
-                    success: function(data){ 
-                     
+                    success: function(data){                      
                         if(data.code==0) {
                                     alert(data.message);
                         }  
                         else {
-                            $(".addprofile").css("display", "none");
+                            $(".add_agent").css("display", "none");
                             $("#last_step").css("display", "block");
                         }      
             }
