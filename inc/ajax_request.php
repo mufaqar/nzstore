cb168af5248b338773f6d394413d42fc13d5d380
Mufaqar Islam <mufaqar@gmail.com>
2022-10-18 21:48:17 +0500

    email

--- inc/ajax_request.php
+++ inc/ajax_request.php
@@ -786,6 +786,14 @@ function agent_create_signup() {
 	  $password = generateRandomString();
 
 
+	  	$admin = "budgetcomputer2013@gmail.com";
+	    $headers = array('Content-Type: text/html; charset=UTF-8');	
+		$headers  = "From: " . $admin . "\r\n";
+		$headers .= "Reply-To: " . $admin . "\r\n";
+		$headers .= "MIME-Version: 1.0\r\n";
+		$headers .= "Content-Type: text/html; charset=UTF-8\r\n";
+
+
 	  $user_data = array(
 				'user_login' => $username,
 				'user_email' => $agent_email,
@@ -805,7 +813,7 @@ function agent_create_signup() {
 		$code = sha1( $user_id . time() );
 		$activation_link = add_query_arg( array( 'key' => $code, 'user' => $user_id ), get_permalink(179));
 		add_user_meta( $user_id, 'has_to_be_activated', $code, true );
-		mail($agent_email, 'ACTIVATION ACCOUNT', 'HERE IS YOUR ACTIVATION LINK: ' . $activation_link );	
+		mail($agent_email, 'ACTIVATION ACCOUNT', 'HERE IS YOUR ACTIVATION LINK: ' . $activation_link , $headers );	
 		echo wp_send_json( array('code' => 200 , 'message'=>__('We have Created an account for you.')));
 	  } else {
 		if (isset($user_id->errors['empty_user_login'])) {	          
