7115fc511426a02f34c4bb611d0e51d5b86d08d9
Mufaqar Islam <mufaqar@gmail.com>
2022-10-18 20:08:51 +0500

    updated

--- footer-admin.php
+++ footer-admin.php
@@ -35,8 +35,7 @@
         buttons: [
             'copyHtml5',
             'excelHtml5',
-            'csvHtml5',
-            'pdfHtml5'
+            'csvHtml5'
         ]
     } );
 } );
--- super/super-users.php
+++ super/super-users.php
@@ -5,7 +5,7 @@ get_header('admin');
 <div class="admin_parrent">
     <div class="toggle_btn">
         <div class="row ">
-            <div class="catering_wrapper mt-5 mb-2 col-md-8 p-0">
+            <div class="catering_wrapper mt-5 mb-2 col-md-12 p-0">
                 <div class="catering_menu buttons">
                     <a id="1" class="showSingle _active" target="1" data="">All Users</a>
                     <a id="2" class="showSingle" target="2" data="Administrator">Administrator</a>
