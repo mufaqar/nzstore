eb22086946a1fc5e17e29a78b35fefe9ad7df27d
Mufaqar Islam <mufaqar@gmail.com>
2022-10-18 21:14:03 +0500

    key

--- inc/ajax_request.php
+++ inc/ajax_request.php
@@ -802,7 +802,7 @@ function agent_create_signup() {
 
 		
 			$code = sha1( $user_id . time() );
-			$activation_link = add_query_arg( array( 'key' => $code, 'user' => $user_id ), get_permalink('/email-confirmation'));
+			$activation_link = add_query_arg( array( 'key' => $code, 'user' => $user_id ), get_permalink(179));
 			add_user_meta( $user_id, 'has_to_be_activated', $code, true );
 			mail( $agent_email, 'ACTIVATION SUBJECT', 'HERE IS YOUR ACTIVATION LINK: ' . $activation_link );
 	   
