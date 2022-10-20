4706ecc5fa275681cafb6ec1c5b8e8693c47ce4d
Mufaqar Islam <mufaqar@gmail.com>
2022-10-20 12:50:00 +0500

    updated

--- temp-confirmation.php
+++ temp-confirmation.php
@@ -4,58 +4,6 @@
  */
 
 get_header('landing');
-global $current_user;
-$logged_user = wp_get_current_user();
-$UIL =  $logged_user->user_login;
-$uid =  $logged_user->ID;
-$url = home_url('dashboard');
-if ( is_user_logged_in() ) {
-
-   
-$user_info = get_userdata($uid);
-$role = $user_info->roles;
-
-
-
-
-
-if (in_array('administrator', $user_info->roles)) {
-    $url = home_url('admin-dashboard');
-    wp_redirect($url);
-    exit();
-    }
-elseif (in_array('agent', $user_info->roles)) {
-    $url = home_url('agent-dashboard'); 
-    wp_redirect($url);
-    exit();
-
-    } 
-    elseif (in_array('technician', $user_info->roles)) {
-        $url = home_url('tech-dashboard'); 
-        wp_redirect($url);
-    exit();
-    
-        } 
-else {
-    $url = home_url('dashboard');
-    wp_redirect($url);
-    exit();
-
-
-}
-
-
-
-} else {
-
-//echo "Not Login" ;
-
-
-}
-
-
-
-
 
 
 ?>
@@ -76,26 +24,9 @@ else {
             </div>
             <div class="col-sm-12 right col-md-6 p-3">
                  <img src="<?php bloginfo('template_directory'); ?>/reources/images/logo.png" alt="" style="max-width:350px" > <br/><br/>
-                <h3>Welcome to Budget Computer & Kiwi Mobiles</h3>
-                <p>1191 Eruera street Rotrua, 3010 </p>
-                <p>DID : 073477044 </p>
-                <form class="login_form" id="loginform">
-                    <div class="form-group">
-                        <label for="username">Email</label>
-                        <input type="email" class="form-control border-0 border-bottom rounded-0"
-                            id="username" aria-describedby="emailHelp" placeholder="abc@example.com" value="" required>                  
-                    </div>
-                    <div class="form-group">
-                        <label for="password">Password</label>
-                        <input type="password" class="form-control border-0 border-bottom rounded-0"
-                            id="password" placeholder="Input your password" required>
-                    </div>
-                    <div class="d-flex justify-content-between align-items-center " style="margin-top: 3rem !important;">
-                        <span>Don't remember your <a href="<?php echo home_url('forget-password'); ?>">Password?</a></span>
-                        <button type="submit" class="green_btn">Login</button>
-                    </div>
-                    
-                </form>
+                   <h3>Thank you for joining us . Please check email and use password that you send you in email</h3> 
+                   <h3><a href="<?php echo home_url(); ?>">Login</a> </h3>                 
+              
             </div>
         </div>
 
