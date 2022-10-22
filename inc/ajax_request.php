e892aaf99e26e6a83ed655c8105873292f5e0594
Mufaqar Islam <mufaqar@gmail.com>
2022-10-22 19:29:17 +0500

    updated

--- inc/ajax_request.php
+++ inc/ajax_request.php
@@ -773,21 +773,7 @@ function agent_create_signup() {
 			);
 
 
-			$user_id = 51;
-
-			$code = sha1( $user_id . time());
-			$activation_link = add_query_arg( array( 'key' => $code, 'user' => $user_id ), get_permalink(179));
-
-
-
-			update_user_meta( $user_id, 'has_to_be_activated', $code );
-
-			activation_mail($agent_email, $activation_link);	
-
-			die();
-
-
-	
+		
 		
 		
 
@@ -797,13 +783,12 @@ function agent_create_signup() {
 		update_user_meta( $user_id,'business_name', $business_name);	 
 		update_user_meta( $user_id,'business_phone', $business_phone);	  
 		update_user_meta( $user_id,'postal_code', $postal_code);
-		
-
-
-
+		$code = sha1( $user_id . time());
+		$activation_link = add_query_arg( array( 'key' => $code, 'user' => $user_id ), get_permalink(179));
+		add_user_meta( $user_id, 'has_to_be_activated', $code, true );
+		activation_mail($agent_email, $activation_link);	
 		echo wp_send_json( array('code' => 200 , 'message'=>__('We have Created an account for you.')));
-	  
-		
+	  	
 		
 	  } else {
 		if (isset($user_id->errors['empty_user_login'])) {	          
