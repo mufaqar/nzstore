b3d60e178567921e16e05758af50652be87ce698
Mufaqar Islam <mufaqar@gmail.com>
2022-10-22 18:57:40 +0500

    account

--- inc/ajax_request.php
+++ inc/ajax_request.php
@@ -748,7 +748,7 @@ function agent_create_signup() {
 		$headers .= "MIME-Version: 1.0\r\n";
         $headers .= "Content-Type: text/html; charset=UTF-8\r\n";
 		$body  = "<p><img src='https://kiwimobiles.co.nz/jobform/wp-content/themes/nzstore/reources//images/logo.png' width='320px'></img></p><hr/> ";
-		$body  .= "<p><strong> HERE IS YOUR ACTIVATION LINK: </strong><a href='$activation_link' >Activate your Account</a> </p> ";
+		$body  .= "<p><strong> Account Activation Link: </strong><a href='$activation_link' >Activate your Account</a> </p> ";
 		$body  .= "<p><strong> DID:   </strong> 09 9508717 </p> ";
 		$body  .= "<p><strong> Email:   </strong>repair@kiwimobiles.co.nz  </p> ";
 
