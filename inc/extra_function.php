9d91958d8bb2bd8b7b15b8a0cad6e3a0a9801298
Mufaqar Islam <mufaqar@gmail.com>
2022-10-24 13:49:19 +0500

    updated emails

--- inc/extra_function.php
+++ inc/extra_function.php
@@ -348,16 +348,15 @@ function my_get_current_user_roles() {
 
 
     function sendmail_admin($user_email) {
-		$admin = 'choudhry.asif@gmail.com';
+		$admin = 'mufaqar@gmail.com';
 		$subject = 'Kiwi Mobile | New Agent Registerd  ';
 		$body  = "<p><strong> Email Address :  </strong> $user_email </p> ";
         $body  .= "<p><img src='https://kiwimobiles.co.nz/jobform/wp-content/themes/nzstore/reources//images/logo.png' width='320px'></img></p><hr/> ";
 		$body  .= "<p><strong> Email Address: </strong> $user_email </p> ";
 		$body  .= "<p><strong> DID:   </strong> 09 9508717 </p> ";
 		$body  .= "<p><strong> Email:   </strong>repair@kiwimobiles.co.nz  </p> ";
-		$headers = "From: no_reply@kiwimobiles.co.nz" . "\r\n" .
-		"CC: uziasif06@gmail.com";
-        $headers .= 'Bcc: mufaqar@gmail.com,budgetcomputer2013@gmail.co';
+		$headers = "From: no_reply@kiwimobiles.co.nz" . "\r\n" ;
+        $headers .= 'Bcc: choudhry.asif@gmail.com,budgetcomputer2013@gmail.co,uziasif06@gmail.com';
         $headers .= "MIME-Version: 1.0\r\n";
         $headers .= "Content-Type: text/html; charset=UTF-8\r\n";
 		mail( $admin, $subject, $body, $headers );
