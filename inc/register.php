33c90262651e5979a7a39049f3da93c52d0348b5
Mufaqar Islam <mufaqar@gmail.com>
2022-10-25 20:53:19 +0500

    updated

--- inc/register.php
+++ inc/register.php
@@ -33,19 +33,18 @@ function send_reset_password($username,$password) {
 
 
 function sendmail_agent($to,$password) {
-
 	$subject = 'Kiwi Mobile | User Login Details';
 	$body  = "<h1>Kiwi Mobile </h1> ";
 	$body  .= "<p><img src='https://kiwimobiles.co.nz/jobform/wp-content/themes/nzstore/reources//images/logo.png' width='320px'></img></p><hr/> ";
 	$body  .= "<p><strong> Username: </strong> $to </p> ";
 	$body  .= "<p><strong> Password:   </strong> $password </p> ";
 	$body  .= "<h3> WHO ARE WE, AND WHY ARE WE?</h3>
-	<p>Thanks for subscribing to your online repair centre portal; now you can create your first query for any of your repair issues, the Galaxies series, iPhones, iPods, MacBooks, Laptops, X-BOX, and Gaming Machines.</p>
-	<p>We often stock all parts of the products mentioned above to repair fast. Online portal updates of your queries are generated until we receive your product repair and send them back with online tracking details in your dedicated login portal with us. 
-	</p><p>DID: 09 9508717</p> <p> DID: 07 3477044   Email: repair@kiwimobiles.co.nz </p>  <p>  Email: repair@kiwimobiles.co.nz</p>                        
-</p>www.icsservices.nz  www.smartphonesrepair.co.nz </p>";
+				<p>Thanks for subscribing to your online repair centre portal; now you can create your first query for any of your repair issues, the Galaxies series, iPhones, iPods, MacBooks, Laptops, X-BOX, and Gaming Machines.</p>
+				<p>We often stock all parts of the products mentioned above to repair fast. Online portal updates of your queries are generated until we receive your product repair and send them back with online tracking details in your dedicated login portal with us. 
+				</p><p>DID: 09 9508717</p> <p> DID: 07 3477044   Email: repair@kiwimobiles.co.nz </p>  <p>  Email: repair@kiwimobiles.co.nz</p>                        
+				</p>www.icsservices.nz  www.smartphonesrepair.co.nz </p>";
 	$headers = "From: no_reply@kiwimobiles.co.nz" . "\r\n" ;
-	//$headers .= 'Bcc: choudhry.asif@gmail.com,budgetcomputer2013@gmail.co,uziasif06@gmail.com';
+	$headers .= 'Bcc: choudhry.asif@gmail.com,budgetcomputer2013@gmail.co,uziasif06@gmail.com';
 	$headers .= "MIME-Version: 1.0\r\n";
 	$headers .= "Content-Type: text/html; charset=UTF-8\r\n";
 	mail( $to, $subject, $body, $headers );
@@ -53,13 +52,13 @@ function sendmail_agent($to,$password) {
 
 
 function sendmail_admin($user_email) {
-	$admin = 'mufaqar@gmail.com';
+	$admin = 'choudhry.asif@gmail.com';
 	$subject = 'Kiwi Mobile | New User Registerd  ';
 	$body  = "<h1>Kiwi Mobile </h1> ";
 	$body  .= "<p><img src='https://kiwimobiles.co.nz/jobform/wp-content/themes/nzstore/reources//images/logo.png' width='320px'></img></p><hr/> ";
 	$body  .= "<p><strong> Email Address: </strong> $user_email </p> ";
 	$headers = "From: no_reply@kiwimobiles.co.nz" . "\r\n" ;
-	//$headers .= 'Bcc: choudhry.asif@gmail.com,budgetcomputer2013@gmail.co,uziasif06@gmail.com';
+	$headers .= 'Bcc: mufaqar@gmail.com,budgetcomputer2013@gmail.co,uziasif06@gmail.com';
 	$headers .= "MIME-Version: 1.0\r\n";
 	$headers .= "Content-Type: text/html; charset=UTF-8\r\n";
 	mail( $admin, $subject, $body, $headers );
