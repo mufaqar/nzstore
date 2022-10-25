d843050133812f5717875cb1d5b7e0a221f1454c
Mufaqar Islam <mufaqar@gmail.com>
2022-10-25 19:51:49 +0500

    updated password

--- inc/register.php
+++ inc/register.php
@@ -1,28 +1,20 @@
 <?php
 
 
-// function generateRandomString($length = 10) {
-// 	$characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-// 	$charactersLength = strlen($characters);
-// 	$randomString = '';
-// 	for ($i = 0; $i < $length; $i++) {
-// 		$randomString .= $characters[rand(0, $charactersLength - 1)];
-// 	}
-// 	return $randomString;
-// }
 
-// function sendmail_signup($to,$password) {
-// 	$to = $to;
-// 	$admin = 'hei@doubledowndish.no';
-// 	$subject = 'Double Down Dish | Username & Password';
-// 	$body  = "<p><strong> Username :  </strong> $to </p> <p> <strong> Password : </strong> $password  </p>";
-// 	$headers = array('Content-Type: text/html; charset=UTF-8');	
-// 	$headers  = "From: " . $admin . "\r\n";
-// 	$headers .= "Reply-To: " . $to . "\r\n";		
-// 	$headers .= "MIME-Version: 1.0\r\n";
-// 	$headers .= "Content-Type: text/html; charset=UTF-8\r\n";
-// 	mail( $to, $subject, $body, $headers );
-// }
+
+function sendmail_signup($to,$password) {
+	$to = $to;
+	$admin = 'no_repley@kiwimobile.co.nz';
+	$subject = 'Kiwi Mobile  | Username & Password';
+	$body  = "<p><strong> Username :  </strong> $to </p> <p> <strong> Password : </strong> $password  </p>";
+	$headers = array('Content-Type: text/html; charset=UTF-8');	
+	$headers  = "From: " . $admin . "\r\n";
+	$headers .= "Reply-To: " . $to . "\r\n";		
+	$headers .= "MIME-Version: 1.0\r\n";
+	$headers .= "Content-Type: text/html; charset=UTF-8\r\n";
+	mail( $to, $subject, $body, $headers );
+}
 
 
 
@@ -78,36 +70,36 @@
 
 
 
-// add_action('wp_ajax_resetpassword', 'resetpassword', 0);
-// add_action('wp_ajax_nopriv_resetpassword', 'resetpassword');
-// function resetpassword() {	
-// 	  $username = stripcslashes($_POST['username']);	
-// 	  $password = generateRandomString();		
-// 	  global $wpdb;  
-//     //We shall SQL escape all inputs  
-//       $username = $_POST['username'];
-//       $email = $_POST['username'];    
-// 	  $password = generateRandomString();	
-// 	  $user_data = array(
-// 		'user_login' => $username,
-// 		'user_email' => $username,
-// 		'user_pass' => $password,	
+add_action('wp_ajax_resetpassword', 'resetpassword', 0);
+add_action('wp_ajax_nopriv_resetpassword', 'resetpassword');
+function resetpassword() {	
+	  $username = stripcslashes($_POST['username']);	
+	  $password = generateRandomString();		
+	  global $wpdb;  
+    //We shall SQL escape all inputs  
+      $username = $_POST['username'];
+      $email = $_POST['username'];    
+	  $password = generateRandomString();	
+	  $user_data = array(
+		'user_login' => $username,
+		'user_email' => $username,
+		'user_pass' => $password,	
 		
-// 		);
+		);
 
-// 		$user = get_user_by( 'email', $email );
-// 		$user_id = $user->ID;
-// 	    $user_id = wp_update_user( array ( 'ID' => $user_id, 'user_pass' => $password ) );	
-// 	  	if (!is_wp_error($user_id)) {		    
-// 			sendmail($username,$password);
-// 			echo wp_send_json( array('code' => 200 , 'message'=>__('Password Updated , Please check your email')));
-// 	  	} else {	    		         
-// 			  echo wp_send_json( array('code' => 0 , 'message'=>__('Error Occured please check your email address')));
-// 	      	}
+		$user = get_user_by( 'email', $email );
+		$user_id = $user->ID;
+	    $user_id = wp_update_user( array ( 'ID' => $user_id, 'user_pass' => $password ) );	
+	  	if (!is_wp_error($user_id)) {		    
+			sendmail($username,$password);
+			echo wp_send_json( array('code' => 200 , 'message'=>__('Password Updated , Please check your email')));
+	  	} else {	    		         
+			  echo wp_send_json( array('code' => 0 , 'message'=>__('Error Occured please check your email address')));
+	      	}
 	  	
-// 	die;   	
+	die;   	
 		
-// }
+}
 
 
 
