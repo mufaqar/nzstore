8a4a826fc2448d489b74498a0dc651a1a5899800
Mufaqar Islam <mufaqar@gmail.com>
2022-10-26 13:13:15 +0500

    udpated

--- inc/register.php
+++ inc/register.php
@@ -8,7 +8,7 @@ function sendmail($agent_email,$message,$postid) {
 	// $headers .= 'Bcc: choudhry.asif@gmail.com';
 	// $headers .= 'Bcc: budgetcomputer2013@gmail.co';
 	// $headers .= 'Bcc: uziasif06@gmail.com';
-	$headers .= 'Bcc: mufaqar@gmail.com';
+	$headers .= 'Cc: mufaqar@gmail.com';
 	$headers .= "MIME-Version: 1.0\r\n";
 	$headers .= "Content-Type: text/html; charset=UTF-8\r\n";
 	$body   = "<p><img src='https://kiwimobiles.co.nz/jobform/wp-content/themes/nzstore/reources//images/logo.png' width='320px'></img></p><hr/> ";
