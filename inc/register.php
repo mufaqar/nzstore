6f39f444cc67b0eae3529e8f90708860efb36ff9
Mufaqar Islam <mufaqar@gmail.com>
2022-10-27 13:39:30 +0500

    updated

--- inc/register.php
+++ inc/register.php
@@ -5,9 +5,9 @@ function sendmail($agent_email,$message,$postid) {
 	
 	$subject = "Budget Computer & Kiwi Mobiles  |  $message ";
 	$headers[] = 'From: budgetcomputer@kiwimobiles.co.nz" . "\r\n';
-	$headers[] = 'Cc: choudhry.asif@gmail.com';
-	$headers[] = 'Cc: budgetcomputer2013@gmail.co';
-	$headers[] = 'Cc: uziasif06@gmail.com';
+	$headers[] = 'Bcc: choudhry.asif@gmail.com';
+	$headers[] = 'Bcc: budgetcomputer2013@gmail.co';
+	$headers[] = 'Bcc: uziasif06@gmail.com';
 	$headers[] = "Content-Type: text/html; charset=UTF-8\r\n";
 	$body   = "<p><img src='https://kiwimobiles.co.nz/jobform/wp-content/themes/nzstore/reources//images/logo.png' width='320px'></img></p><hr/> ";
 	$body  .= "<p><strong> $message  </strong> <br/> Ticket   :  ".get_permalink($postid)."  </p>";	
@@ -50,7 +50,7 @@ function sendmail_agent($to,$password) {
 	$body  .= "<p><img src='https://kiwimobiles.co.nz/jobform/wp-content/themes/nzstore/reources//images/logo.png' width='320px'></img></p><hr/> ";
 	$body  .= "<p><strong> Username: </strong> $to </p> ";
 	$body  .= "<p><strong> Password:   </strong> $password </p> ";
-	$body  .= "<h1 style='color:#5fb227;margin:20px 0;'> YOUR OLINE REPAIR PARTNER </h1>";
+	$body  .= "<h1 style='color:#5fb227;margin:20px 0;'> YOUR ONLINE REPAIR PARTNER </h1>";
 	$body  .= "<p>Thanks for subscribing to your online repair centre portal; now you can create your first query for any of your repair issues, the Galaxies series, iPhones, iPods, MacBooks, Laptops, X-BOX, and Gaming Machines. </p>";
 	$body  .= "<p>We often stock all parts of the products mentioned above to repair fast. Online portal updates of your queries are generated until we receive your product repair and send them back with online tracking details in your dedicated login portal with us.  </p>";
 	$body  .= "<p><strong> Email: </strong>  repair@kiwimobiles.co.nz </p> ";
