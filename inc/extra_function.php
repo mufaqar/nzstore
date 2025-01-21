009180ce1563e5f037368f6f9bca81b72ebfaf85
mufaqar <mufaqar@gmail.com>
2025-01-21 15:21:51 +0500

    updated

--- inc/extra_function.php
+++ inc/extra_function.php
@@ -342,8 +342,7 @@ function activation_mail($to,$activation_link) {
 
     // Reset Password Email 
 
-function Send_Password($username,$password) {
-    echo $username;
+function Send_Password($username,$password) {   
     $subject = 'Budget Computer & Kiwi Mobiles | Reset Password';	
     $headers[] = 'From: Budget Computer <budgetcomputer@kiwimobiles.co.nz>';
     $body   = "<p><img src='https://kiwimobiles.co.nz/jobform/wp-content/themes/nzstore/reources/images/logo.png' width='320px'></img></p><hr/> ";
