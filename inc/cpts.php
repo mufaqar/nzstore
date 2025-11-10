81b77f034392224139ad91af95a4f711d59dfcef
Mufaqar <mufaqar@gmail.com>
2025-11-10 17:46:45 +0500

    nivoice

--- inc/cpts.php
+++ inc/cpts.php
@@ -437,6 +437,50 @@ function cptui_register_my_taxes_ticket_fault_type() {
 	}
 	//add_action( 'init', 'cptui_register_my_taxes_model_cat' );
 
+
+	function cptui_register_my_cpts_invoice() {
+
+	/**
+	 * Post Type: Invoices.
+	 */
+
+	$labels = [
+		"name" => esc_html__( "Invoices", "NZ Store" ),
+		"singular_name" => esc_html__( "Invoice", "NZ Store" ),
+	];
+
+	$args = [
+		"label" => esc_html__( "Invoices", "NZ Store" ),
+		"labels" => $labels,
+		"description" => "",
+		"public" => true,
+		"publicly_queryable" => true,
+		"show_ui" => true,
+		"show_in_rest" => true,
+		"rest_base" => "",
+		"rest_controller_class" => "WP_REST_Posts_Controller",
+		"rest_namespace" => "wp/v2",
+		"has_archive" => false,
+		"show_in_menu" => true,
+		"show_in_nav_menus" => true,
+		"delete_with_user" => false,
+		"exclude_from_search" => false,
+		"capability_type" => "post",
+		"map_meta_cap" => true,
+		"hierarchical" => false,
+		"can_export" => false,
+		"rewrite" => [ "slug" => "invoice", "with_front" => true ],
+		"query_var" => true,
+		"supports" => [ "title", "editor", "thumbnail" ],
+		"show_in_graphql" => false,
+	];
+
+	register_post_type( "invoice", $args );
+}
+
+add_action( 'init', 'cptui_register_my_cpts_invoice' );
+
+
 	
 
 
